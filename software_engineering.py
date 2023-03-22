4e4b9177a803488e99ea5242d87ae3c5f294118c
Tjmcooper <127441625+Tjmcooper@users.noreply.github.com>
2023-03-22 13:33:31 -0400

    decode function and minor change

--- software_engineering.py
+++ software_engineering.py
@@ -1,27 +1,29 @@
 # Ryleigh Sperry
 
-def print_menu(): #print menu function
+def print_menu():  # print menu function
     print("Menu")
     print("-------------")
     print("1. Encode")
     print("2. Decode")
     print("3. Quit")
 
-def encode(password): # create an encoding function that iterates through 
+
+def encode(password):  # create an encoding function that iterates through
+    global new_password
     new_password = ""
     list(password)
     for i in password:
         i = int(i)
-        i += 3
+        i = (i + 3) % 10 #changed this so its only 1 digit after encoding
         i = str(i)
         new_password += i
-
 
     return new_password
 
 
-
-
+def decode():
+    print(f'The encoded password is {new_password} amd the original password is {password}')
+    return
 
 
 if __name__ == "__main__":
@@ -29,11 +31,9 @@ if __name__ == "__main__":
     while program == True:
         option = int(input("Please enter an option:"))
         if option == 1:
+            global password
             password = input("Please enter your password to encode:")
-            password = encode(password)
+            new_password = encode(password)
             print("Your password has been encoded and stored!")
-
-
-
-
-
+        elif option == 2:
+            decode()
